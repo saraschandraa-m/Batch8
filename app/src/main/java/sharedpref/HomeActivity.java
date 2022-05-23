c75c02463352e151dc209e0619b422e31da270f7
Saraschandraa Muralisekar <saraschandraa.naidu@hotmail.com>
2022-05-23 14:58:08 +0530

    Shared Pref 23May2022

--- app/src/main/java/sharedpref/HomeActivity.java
+++ app/src/main/java/sharedpref/HomeActivity.java
@@ -2,15 +2,37 @@ package sharedpref;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.appstone.androidbatch8.R;
 
 public class HomeActivity extends AppCompatActivity {
 
+
+    SharedPreferences pref;
+    SharedPreferences.Editor editor;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        pref = getApplicationContext().getSharedPreferences("APPSTONE", MODE_PRIVATE);
+        editor = pref.edit();
+    }
+
+    public void doLogoutClicked(View v1){
+        editor.putString("EMAIL", "");
+        editor.putString("PASSWORD", "");
+        editor.putBoolean("ISLOGIN", false);
+        editor.apply();
+
+        Intent loginIntent = new Intent(HomeActivity.this, LoginActivity.class);
+        startActivity(loginIntent);
+        finish();
     }
 }

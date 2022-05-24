f518e531871efae0b003bed3fdeed41cfaa19ef5
Saraschandraa Muralisekar <saraschandraa.naidu@hotmail.com>
2022-05-24 15:01:15 +0530

    Explicit Intent added 24May2021

--- app/src/main/java/sharedpref/HomeActivity.java
+++ app/src/main/java/sharedpref/HomeActivity.java
@@ -2,11 +2,14 @@ package sharedpref;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import com.appstone.androidbatch8.R;
 
@@ -15,6 +18,7 @@ public class HomeActivity extends AppCompatActivity {
 
     SharedPreferences pref;
     SharedPreferences.Editor editor;
+    private EditText displayText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,16 +27,33 @@ public class HomeActivity extends AppCompatActivity {
 
         pref = getApplicationContext().getSharedPreferences("APPSTONE", MODE_PRIVATE);
         editor = pref.edit();
+
+
+        Bundle newData = getIntent().getExtras();
+
+        String email = newData.getString("EMAIL");
+        String password = newData.getString("PASSWORD");
+
+        displayText = findViewById(R.id.textView7);
+
+        displayText.setText(email);
     }
 
     public void doLogoutClicked(View v1){
-        editor.putString("EMAIL", "");
-        editor.putString("PASSWORD", "");
-        editor.putBoolean("ISLOGIN", false);
-        editor.apply();
+//        editor.putString("EMAIL", "");
+//        editor.putString("PASSWORD", "");
+//        editor.putBoolean("ISLOGIN", false);
+//        editor.apply();
+//
+//        Intent loginIntent = new Intent(HomeActivity.this, LoginActivity.class);
+//        startActivity(loginIntent);
+//        finish();
 
-        Intent loginIntent = new Intent(HomeActivity.this, LoginActivity.class);
-        startActivity(loginIntent);
+        String newEmail = displayText.getText().toString();
+
+        Intent returnIntent = new Intent();
+        returnIntent.putExtra("EMAIL", newEmail);
+        setResult(Activity.RESULT_OK, returnIntent);
         finish();
     }
 }
--- app/src/main/java/sharedpref/LoginActivity.java
+++ app/src/main/java/sharedpref/LoginActivity.java
@@ -1,13 +1,16 @@
 package sharedpref;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import com.appstone.androidbatch8.R;
 
@@ -18,6 +21,9 @@ public class LoginActivity extends AppCompatActivity {
 
     SharedPreferences preferences;
     SharedPreferences.Editor editor;
+
+    private String email;
+    private String password;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,20 +45,35 @@ public class LoginActivity extends AppCompatActivity {
     }
 
     public void onLoginButtonClicked(View v) {
-        String email = etEmail.getText().toString();
-        String password = etPassword.getText().toString();
+        email = etEmail.getText().toString();
+        password = etPassword.getText().toString();
 
-        editor.putString("EMAIL", email);
-        editor.putString("PASSWORD", password);
-        editor.putBoolean("ISLOGIN", true);
-        editor.apply();
+//        editor.putString("EMAIL", email);
+//        editor.putString("PASSWORD", password);
+//        editor.putBoolean("ISLOGIN", true);
+//        editor.apply();
 
         moveToHomeScreen();
     }
 
     private void moveToHomeScreen() {
         Intent homeIntent = new Intent(LoginActivity.this, HomeActivity.class);
-        startActivity(homeIntent);
-        finish();
+        homeIntent.putExtra("EMAIL", email);
+        homeIntent.putExtra("PASSWORD", password);
+        startActivityForResult(homeIntent, 124);
+//        finish();
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode == 124){
+            if(resultCode == Activity.RESULT_OK){
+                String newEmail = data.getExtras().getString("EMAIL");
+                etEmail.setText(newEmail);
+            }else{
+                Toast.makeText(LoginActivity.this, "User Cancelled", Toast.LENGTH_SHORT).show();
+            }
+        }
     }
 }

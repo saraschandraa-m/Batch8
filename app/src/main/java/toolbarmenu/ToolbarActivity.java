aed0d12ef5400595b4ef2f21b6a3dfce06d5af86
Saraschandraa Muralisekar <saraschandraa.naidu@hotmail.com>
2022-05-31 11:43:45 +0530

    Navigation Drawer 31May2022

--- app/src/main/java/toolbarmenu/ToolbarActivity.java
+++ app/src/main/java/toolbarmenu/ToolbarActivity.java
@@ -1,16 +1,25 @@
 package toolbarmenu;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.appstone.androidbatch8.R;
+import com.google.android.material.navigation.NavigationView;
 
-public class ToolbarActivity extends AppCompatActivity {
+public class ToolbarActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+
+    private DrawerLayout mDrawerLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,9 +30,37 @@ public class ToolbarActivity extends AppCompatActivity {
 
         setSupportActionBar(toolbar);
 
-        if(getSupportActionBar() != null){
-            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().setDisplayHomeAsUpEnabled(false);
         }
+
+
+        mDrawerLayout = findViewById(R.id.drawer_layout);
+
+        ActionBarDrawerToggle drawerToggle = new ActionBarDrawerToggle(ToolbarActivity.this, mDrawerLayout, toolbar,
+                R.string.open, R.string.close);
+        drawerToggle.syncState();
+
+        NavigationView navigationView = findViewById(R.id.navigation_view);
+
+        navigationView.setNavigationItemSelectedListener(this);
+
+        View headerView = navigationView.getHeaderView(0);
+
+        TextView usernameText = headerView.findViewById(R.id.textView8);
+        usernameText.setText("Hello Android Developer");
+
+//        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+//            @Override
+//            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+//                if(item.getItemId() == R.id.menu_logout){
+//                    Toast.makeText(ToolbarActivity.this, "Menu Logout is clicked", Toast.LENGTH_SHORT).show();
+//                }
+//                return false;
+//            }
+//        });
+
+
     }
 
     @Override
@@ -34,9 +71,31 @@ public class ToolbarActivity extends AppCompatActivity {
 
     @Override
     public boolean onOptionsItemSelected(@NonNull MenuItem item) {
-        if(item.getItemId() == android.R.id.home){
-            onBackPressed();
+//        if (item.getItemId() == android.R.id.home) {
+//            onBackPressed();
+//        } else if (item.getItemId() == R.id.menu_logout) {
+//            Toast.makeText(ToolbarActivity.this, "Logout is clicked", Toast.LENGTH_SHORT).show();
+//        }
+
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                onBackPressed();
+                break;
+
+            case R.id.menu_logout:
+                Toast.makeText(ToolbarActivity.this, "Logout is clicked", Toast.LENGTH_SHORT).show();
+                break;
         }
+
         return true;
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        mDrawerLayout.closeDrawer(GravityCompat.START);
+        if(item.getItemId() == R.id.menu_logout){
+            Toast.makeText(ToolbarActivity.this, "Menu Logout is clicked", Toast.LENGTH_SHORT).show();
+        }
+        return false;
     }
 }

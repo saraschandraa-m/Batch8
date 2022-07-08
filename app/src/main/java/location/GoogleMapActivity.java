04b4fad74b9baa827494509b48eab7b035cb3e1e
Saraschandraa Muralisekar <saraschandraa.naidu@hotmail.com>
2022-07-08 11:14:38 +0530

    Map and Location Added 08Jul2022

--- app/src/main/java/location/GoogleMapActivity.java
+++ app/src/main/java/location/GoogleMapActivity.java
@@ -1,31 +1,117 @@
 package location;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
+import android.annotation.SuppressLint;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
+import android.widget.Toast;
 
 import com.appstone.androidbatch8.R;
+import com.google.android.gms.maps.CameraUpdate;
+import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 
-public class GoogleMapActivity extends AppCompatActivity {
+import java.util.List;
 
-    private GoogleMap googleMap;
+public class GoogleMapActivity extends AppCompatActivity implements LocationListener {
+
+    private GoogleMap mGoogleMap = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_google_map);
 
+        //Setting up the Map Fragment
         SupportMapFragment mapFragment = SupportMapFragment.newInstance();
 
-        getSupportFragmentManager().beginTransaction().replace(R.id.map_fragment, mapFragment);
+        //Initiating the map Fragment in the FrameLayout
+        getSupportFragmentManager().beginTransaction().replace(R.id.map_fragment, mapFragment).commit();
+
+
         mapFragment.getMapAsync(new OnMapReadyCallback() {
             @Override
             public void onMapReady(GoogleMap googleMap) {
-
+                mGoogleMap = googleMap;
+                checkForLocationPermission();
             }
         });
+    }
+
+    private void checkForLocationPermission() {
+        if (ContextCompat.checkSelfPermission(GoogleMapActivity.this, Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED
+                && ContextCompat.checkSelfPermission(GoogleMapActivity.this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+            // If persmission Granted
+            findMyLocation();
+        } else {
+            //If Permission not granted.
+            ActivityCompat.requestPermissions(GoogleMapActivity.this,
+                    new String[]{Manifest.permission.ACCESS_COARSE_LOCATION, Manifest.permission.ACCESS_FINE_LOCATION},
+                    144);
+        }
+    }
+
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == 144) {
+            if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                //permission is granted
+                findMyLocation();
+            } else {
+                Toast.makeText(GoogleMapActivity.this, "User did not give permission", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
+    @SuppressLint("MissingPermission")
+    private void findMyLocation() {
+        LocationManager lm = (LocationManager) getSystemService(LOCATION_SERVICE);
+        lm.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 10, 10, GoogleMapActivity.this);
+    }
+
+    @Override
+    public void onLocationChanged(@NonNull Location location) {
+        double latitude = location.getLatitude();
+        double longitude = location.getLongitude();
+
+        LatLng currentLocation = new LatLng(latitude, longitude);
+
+        mGoogleMap.addMarker(new MarkerOptions()
+                .position(currentLocation)
+                .title("My Location"));
+
+
+        //6.0f - 15.0f
+
+        mGoogleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(currentLocation, 14.0f));
+    }
+
+    @Override
+    public void onLocationChanged(@NonNull List<Location> locations) {
+        LocationListener.super.onLocationChanged(locations);
+    }
+
+    @Override
+    public void onProviderEnabled(@NonNull String provider) {
+        LocationListener.super.onProviderEnabled(provider);
+    }
+
+    @Override
+    public void onProviderDisabled(@NonNull String provider) {
+        LocationListener.super.onProviderDisabled(provider);
     }
 }
